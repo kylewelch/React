05b2c4e4e8d355d3cec8e34e159bc078bd117cdd
KyleWelch1 <>
2023-01-04 23:52:24 +0000

    finished this section

--- src/ContinueButton.jsx
+++ src/ContinueButton.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
-export default function ContinueButton({ selectionMade }) {
+export default function ContinueButton({ selectionMade, nextQuestion }) {
   return (
-    <button className={selectionMade ? "ContinueButton" : "ContinueButton-disabled"}>CONTINUE</button>
+    <button className={selectionMade ? "ContinueButton" : "ContinueButton-disabled"} onClick={selectionMade ? nextQuestion : null}>CONTINUE</button>
   )
 }
--- src/Page.jsx
+++ src/Page.jsx
@@ -4,24 +4,29 @@ import Answers from './Answers'
 import ContinueButton from './ContinueButton'
 
 export default function Page() {
+  const questions = ["Question 1?", "Question 2?"];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   function updateSelection(answer) {
     setSelectedAnswer(answer);
   }
-  
+  function nextQuestion() {
+    setCurrentQuestion(currentQuestion + 1);
+    setSelectedAnswer(null);
+  }
   return (
     <div>
-      
       <div className="progress">
         <div className="progress-bar"></div>
       </div>
-      <h1>Testing this out</h1>
+      <h1>{questions[currentQuestion]}</h1>
       <Answers 
         updateSelection={updateSelection}
         selectedAnswer={selectedAnswer}
       />
       <ContinueButton 
         selectionMade={selectedAnswer !== null} 
+        nextQuestion={nextQuestion}
       />
     </div>
   )
