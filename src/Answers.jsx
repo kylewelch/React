d840c67577e61efdd7276627aa766f48ca0ee238
KyleWelch1 <>
2023-01-05 01:02:13 +0000

    finished v1 of module 13

--- src/Answers.jsx
+++ src/Answers.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Answer from './Answer'
 
-export default function Answers({updateSelection, selectedAnswer}) {
-  const answers = ['one', 'two', 'three']
+export default function Answers({updateSelection, selectedAnswer, currentQuestion}) {
+  const answers = ['one', 'two', 'three'];
+  const answers2 = ['four', 'five', 'six'];
+  const allAnswers = [answers, answers2];
   return (
     <div>
       { // for every item in the 'answers' list, create an Answer component
-        answers.map((answer, index) => {
+        allAnswers[currentQuestion].map((answer, index) => {
       return (
         <Answer 
           age={18}
--- src/Page.jsx
+++ src/Page.jsx
@@ -4,7 +4,7 @@ import Answers from './Answers'
 import ContinueButton from './ContinueButton'
 
 export default function Page() {
-  const questions = ["Question 1?", "Question 2?"];
+  const questions = ["Question 1?", "Question 2?", "Woohoo! You did it!"];
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   function updateSelection(answer) {
@@ -20,10 +20,14 @@ export default function Page() {
         <div className="progress-bar"></div>
       </div>
       <h1>{questions[currentQuestion]}</h1>
-      <Answers 
-        updateSelection={updateSelection}
-        selectedAnswer={selectedAnswer}
-      />
+      {currentQuestion === (questions.length - 1) ? 
+        <img className="celebration" src="https://i.imgur.com/ejNYw2X.png" alt="Celebration of your success" /> :
+        <Answers 
+          updateSelection={updateSelection}
+          selectedAnswer={selectedAnswer}
+          currentQuestion={currentQuestion}
+        />
+      }
       <ContinueButton 
         selectionMade={selectedAnswer !== null} 
         nextQuestion={nextQuestion}
